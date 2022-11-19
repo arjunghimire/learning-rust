04362c68fdd082c0b66dbb26c437fb7f14bc10ad
Arjun Ghimire <arjunghimire0714@gmail.com>
2022-11-19 21:17:38 +0545

    Initial Commit

---
+++ src/formatted_print.rs
@@ -0,0 +1,33 @@
+fn formatted_print() {
+    // In general the `{}` will be automatically replaced with any
+    // arguments. Thes will be stringified.
+
+    println!("{} days", 31);
+
+    // Positional arguments can be used. Specifying an integer inside `{}`
+    // determines which additional argument will be replaced. Arguments start
+    // at 0 immediately after the format string
+
+    println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
+
+    // As can named arguments
+    println!(
+        "{subject} {verb} {object}",
+        object = "the lazy dog",
+        subject = "the quick brown fox",
+        verb = "jumps over"
+    );
+
+    // Different formatting can be invoked by specifying the format character after a
+    // `:`
+
+    println!("Base 10:                     {}", 69420); // 69420
+    println!("Base 2 (binary):             {:b}", 69420); //10000111100101100
+    println!("Base 8 (octal):              {:o}", 69420); //207454
+    println!("Base 16 (hexadecimal):       {:x}", 69420); // 10f2c
+    println!("Base 16 (hexadecimal):       {:X}", 69420); //10F2C
+
+    // You can right-justify text with a specfied width. This will
+    // output "     1". (Four white spaces and a "1", for a total width of 5.)
+    println!("{number:>5}", number = 1);
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod comment;
+mod formatted_print;
+
+fn main() {
+    println!("Hello, world!");
+}
